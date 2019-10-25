98d3a9ff228ca580288228ff360a249dab7aaf9b
Ljerkj <ljerkj@mail.com>
2019-10-25 16:10:00 +0700

    create test script

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+var="the quick brown fox jumps over the lazy dog "
+echo "Input: $var"
+echo "Expected output:
+brown  1
+dog  1
+fox  1
+jumps  1
+lazy  1
+over  1
+quick  1
+the  2"
+echo "Actual output:"
+java -jar trainmyself.jar $var
