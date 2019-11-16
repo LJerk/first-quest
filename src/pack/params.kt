5a719c6091c1d36d8b7089b65238d2b0099f93c1
Ljerkj <ljerkj@mail.com>
2019-11-16 14:35:20 +0700

    params refactor

--- src/pack/params.kt
+++ src/pack/params.kt
@@ -12,26 +12,21 @@ class Params(args: Array<String>) {
                 login = null.toString()
                 pass = null.toString()
             }
-            args[0] == "-h" ->{
+            args[0] == "-h" -> {
                 help = true
                 login = null.toString()
                 pass = null.toString()
             }
-            args.size == 4 ->
-                if ((args[0] == "-login" && args[2] == "-pass") || (args[0] == "-pass" && args[2] == "-login")){
-                    help = false
-                    if (args[0] == "-login") {
-                        login = args[1]
-                        pass = args[3]
-                    } else {
-                        login = args[3]
-                        pass = args[1]
-                    }
-                } else {
-                    help = true
-                    login = null.toString()
-                    pass = null.toString()
-                }
+            args[0] == "-login" -> {
+                help = !(args.size == 4 && ((args[0] == "-login" && args[2] == "-pass") || (args[0] == "-pass" && args[2] == "-login")))
+                login = args[1]
+                pass = args[3]
+            }
+            args[0] == "-pass" -> {
+                help = !(args.size == 4 && ((args[0] == "-login" && args[2] == "-pass") || (args[0] == "-pass" && args[2] == "-login")))
+                login = args[3]
+                pass = args[1]
+            }
             else -> {
                 help = true
                 login = null.toString()
