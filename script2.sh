7470d93d16a5c1af5ee5852e9b0b62081f2699f1
Ljerkj <ljerkj@mail.com>
2019-12-07 14:00:03 +0700

    fix sh script

--- script2.sh
+++ script2.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 echo "begin compile"
-kotlinc -verbose src/pack/Login.kt src/pack/User.kt src/pack/Params.kt src/pack/Validate.kt -include-runtime  -d test2.jar
+kotlinc -verbose src -include-runtime  -d test2.jar
 echo "end compile"
 
 echo "Expected output: 255"
-java -jar test2.jar -h
+java -cp test2.jar pack.LoginKt  -h
 echo "Actual output: $?"
 
 echo "Expected output: 0"
-java -jar test2.jar -login admin -pass admin -res AB -role WRITE
+java -cp test2.jar pack.LoginKt  -login admin -pass admin -res AB -role WRITE
 echo "Actual output: $?"
 
 echo "Expected output: 2"
-java -jar test2.jar -login @ -pass admin -res AB -role WRITE
+java -cp test2.jar pack.LoginKt   -login @ -pass admin -res AB -role WRITE
 echo "Actual output: $?"
 
 echo "Expected output: 3"
-java -jar test2.jar -login addmin -pass admin -res AB -role WRITE
+java -cp test2.jar pack.LoginKt   -login addmin -pass admin -res AB -role WRITE
 echo "Actual output: $?"
 
 echo "Expected output: 4"
-java -jar test2.jar -login admin -pass dsa2 -res AB -role WRITE
+java -cp test2.jar pack.LoginKt  -login admin -pass dsa2 -res AB -role WRITE
 echo "Actual output: $?"
